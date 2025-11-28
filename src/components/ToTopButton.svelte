a470ea020268cbaf504cd61dbcfc8c35f812f687
Mohammed Sajid Shaik <sajid.shaik1186@gmail.com>
2025-11-28 17:43:44 +0530

    feat: Added ToTopButton

---
+++ src/components/ToTopButton.svelte
@@ -0,0 +1,21 @@
+<script>
+  import { onMount } from "svelte";
+  let show = false;
+  onMount(() => {
+    window.addEventListener("scroll", () => {
+      show = window.scrollY > 300;
+    });
+  });
+  function goTop() {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  }
+</script>
+
+<button
+  id="to-top-btn"
+  class="contact-button to-top-btn {show ? 'show' : ''}"
+  title="Back to Top"
+  on:click={goTop}
+>
+  <i class="bx bx-arrow-to-top"></i>
+</button>
