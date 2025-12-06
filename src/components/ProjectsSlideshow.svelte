ad846f45be7644c626e98db9da340f3cc411553f
Mohammed Sajid Shaik <sajid.shaik1186@gmail.com>
2025-12-06 10:49:38 +0530

    fix: wierd overlay on slideshow

--- src/components/ProjectsSlideshow.svelte
+++ src/components/ProjectsSlideshow.svelte
@@ -1,5 +1,4 @@
 <script>
-  import { onMount } from "svelte";
   let current = 0;
   const slides = [
     {
@@ -15,28 +14,10 @@
       caption: "Project: Mountain",
     },
   ];
-  const projectDescriptions = [
-    "RippleKit (archived)",
-    "Poly (archived)",
-    "Project: Mountain (archived)",
-  ];
 
   function next(n) {
     current = (current + n + slides.length) % slides.length;
-    updateDesc();
   }
-  function goTo(n) {
-    current = n % slides.length;
-    updateDesc();
-  }
-  let desc = projectDescriptions[0];
-  function updateDesc() {
-    desc = projectDescriptions[current];
-  }
-
-  onMount(() => {
-    updateDesc();
-  });
 </script>
 
 <div class="slideshow-container" style="font-weight: 100;">
@@ -49,17 +30,4 @@
   <a class="prev" on:click={() => next(-1)}>&#10094;</a>
   <a class="next" on:click={() => next(1)}>&#10095;</a>
 
-  <div style="text-align:center; margin-top:15px;">
-    {#each slides as _, i}
-      <span class="dot {i === current ? 'active' : ''}" on:click={() => goTo(i)}
-      ></span>
-    {/each}
-  </div>
-
-  <p
-    id="project-desc"
-    style="text-align:center; margin:1.5rem 0 0 0; font-size:1.1rem; font-weight:100;"
-  >
-    {desc}
-  </p>
 </div>
